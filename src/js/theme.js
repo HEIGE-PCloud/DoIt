a5f26d51fb53e2596656c049d6eb7fefba48e199
HEIGE-PCloud <heige.pcloud@outlook.com>
2021-09-25 13:38:14 +0100

    fix: update window.remark42_config

--- src/js/theme.js
+++ src/js/theme.js
@@ -766,6 +766,7 @@ function initComment() {
                 show_email_subscription: remark42.show_email_subscription,
                 simple_view: remark42.simple_view
             };
+            window.remark_config = remark_config;
             !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);
             window._remark42OnSwitchTheme = (() => {
                 if (window.isDark) {
