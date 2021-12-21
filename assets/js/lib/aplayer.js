15204ccdf99641631ad10cf450d3dcc50038813f
HEIGE-PCloud <heige.pcloud@outlook.com>
2021-12-21 19:47:06 +0000

    chore: remove console.log in prod

--- assets/js/lib/aplayer.js
+++ assets/js/lib/aplayer.js
@@ -4,9 +4,6 @@ import APlayer from 'aplayer'
 // Get all aplayer containers
 const aplayers = document.getElementsByClassName('aplayer-shortcode')
 Array.from(aplayers).forEach(aplayer => {
-  console.log(aplayer)
-  console.log(aplayer.dataset.audio)
-  console.log(aplayer.dataset.options)
   const audio = JSON.parse(aplayer.dataset.audio)
   const options = JSON.parse(aplayer.dataset.options)
   options.audio = audio
