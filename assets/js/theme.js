623414f3e7b09d09ef029f5914ed30d67801d145
PCloud <52968553+HEIGE-PCloud@users.noreply.github.com>
2021-12-25 16:51:21 +0000

    refactor: optimize toc logic (#426)
    * optimize toc logic
    
    - use CSS to set its width instead of js
    - process the logic of static toc and auto toc in hugo template instead of js
    
    * remove initToc from onResize event
    
    - Since the position calcuation is done by CSS (and media query) now, there is no need to initToc onResize
    
    * add scrollIntoView to overflowed toc
    
    * refactor: implement media query

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -551,29 +551,9 @@ function initToc () {
   const isTocStatic = window.matchMedia && window.matchMedia('only screen and (max-width: 1000px)').matches
 
   if (document.getElementById('toc-static').getAttribute('kept') || isTocStatic) {
-    const tocContentStatic = document.getElementById('toc-content-static')
-    if (tocCore.parentElement !== tocContentStatic) {
-      tocCore.parentElement.removeChild(tocCore)
-      tocContentStatic.appendChild(tocCore)
-    }
     if (window._tocOnScroll) window.scrollEventSet.delete(window._tocOnScroll)
   } else {
-    const tocContentAuto = document.getElementById('toc-content-auto')
-    if (tocCore.parentElement !== tocContentAuto) {
-      tocCore.parentElement.removeChild(tocCore)
-      tocContentAuto.appendChild(tocCore)
-    }
-    // The toc element
     const toc = document.getElementById('toc-auto')
-    // The page element
-    const page = document.getElementsByClassName('page')[0]
-    // The rect of the page
-    const rect = page.getBoundingClientRect()
-    // The toc is 20px to the right of the page
-    toc.style.left = `${rect.left + rect.width + 20}px`
-    // The toc occupy all the right of the window
-    toc.style.maxWidth = `${window.innerWidth - rect.right - 20}px`
-    toc.style.visibility = 'visible'
     const tocLinkElements = tocCore.querySelectorAll('a:first-child')
     const tocLiElements = tocCore.getElementsByTagName('li')
     const headerLinkElements = document.getElementsByClassName('headerLink')
@@ -632,6 +612,10 @@ function initToc () {
       // and all its parent to has-active
       if (activeTocIndex >= 0 && activeTocIndex < tocLinkElements.length) {
         tocLinkElements[activeTocIndex].classList.add('active')
+        tocLinkElements[activeTocIndex].scrollIntoView({
+          behavior: 'smooth',
+          block: 'center'
+        })
         let parent = tocLinkElements[activeTocIndex].parentElement
         while (parent !== tocCore) {
           parent.classList.add('has-active')
@@ -813,7 +797,6 @@ function onResize () {
       window._resizeTimeout = window.setTimeout(() => {
         window._resizeTimeout = null
         for (const event of window.resizeEventSet) event()
-        initToc()
         initSearch()
       }, 100)
     }
@@ -887,7 +870,7 @@ document.addEventListener('pjax:send', function () {
   delete window._tocOnScroll
   const el = document.getElementById('content')
   if (el) {
-    window.lgData[el?.getAttribute('lg-uid')].destroy(true)
+    window.lgData[el?.getAttribute('lg-uid')]?.destroy(true)
   }
 })
 
