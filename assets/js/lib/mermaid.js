1a9ebd733fd946e57d78d3d38a96503612e20b51
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-02-17 10:53:51 +0000

    fix: use mermaidAPI
    Signed-off-by: HEIGE-PCloud <heige.pcloud@outlook.com>

--- assets/js/lib/mermaid.js
+++ assets/js/lib/mermaid.js
@@ -2,7 +2,13 @@ import mermaid from 'mermaid'
 
 const mermaidElements = document.getElementsByClassName('mermaid')
 if (mermaidElements.length) {
-  mermaid.initialize({
-    startOnLoad: true
+  mermaid.initialize({ startOnLoad: false, theme: 'default' })
+  console.log(mermaid)
+
+  Array.from(mermaidElements).forEach((e) => {
+    mermaid.mermaidAPI.render('svg-' + e.id, window.config.data[e.id], svgCode => {
+      e.insertAdjacentHTML('afterbegin', svgCode)
+      document.getElementById('svg-' + e.id).children[0].remove()
+    }, e)
   })
 }
