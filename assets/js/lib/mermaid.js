8e95219ee9f61db43736d81cb5299ff9dcd0fa61
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-02-17 10:25:04 +0000

    refactor: add bundle option support for mermaidjs
    Signed-off-by: HEIGE-PCloud <heige.pcloud@outlook.com>

--- assets/js/lib/mermaid.js
+++ assets/js/lib/mermaid.js
@@ -1,13 +1,8 @@
 import mermaid from 'mermaid'
-import mermaidAPI from 'mermaid/mermaidAPI'
 
 const mermaidElements = document.getElementsByClassName('mermaid')
 if (mermaidElements.length) {
-  mermaid.initialize({ startOnLoad: false, theme: 'default' })
-  Array.from(mermaidElements).forEach((mermaid) => {
-    mermaidAPI.render('svg-' + mermaid.id, window.config.data[mermaid.id], svgCode => {
-      mermaid.insertAdjacentHTML('afterbegin', svgCode)
-      document.getElementById('svg-' + mermaid.id).children[0].remove()
-    }, mermaid)
+  mermaid.initialize({
+    startOnLoad: true
   })
 }
