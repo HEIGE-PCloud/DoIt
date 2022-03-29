7e4a16480a478b4b14b25ed60ab349648dfa7d63
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-03-30 00:20:31 +0100

    fix: exclude comment section for katex and render document.body
    Signed-off-by: HEIGE-PCloud <heige.pcloud@outlook.com>

--- assets/js/lib/katex.js
+++ assets/js/lib/katex.js
@@ -1,5 +1,10 @@
 import renderMathInElement from 'katex/dist/contrib/auto-render'
 
 if (window.config?.math && document.getElementById('content')) {
-  renderMathInElement(document.getElementById('content'), window.config.math)
+  // ignore comment section if exists
+  // https://github.com/HEIGE-PCloud/DoIt/issues/487
+  if (document.getElementsByClassName('comment')) {
+    window.config.math.ignoredClasses = ['comment']
+  }
+  renderMathInElement(document.body, window.config.math)
 }
