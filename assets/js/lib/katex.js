9ce02d0e1f09e78b33997dc827a0fae11c377e0d
HEIGE-PCloud <heige.pcloud@outlook.com>
2022-03-30 14:18:17 +0100

    fix: remove incorrect condition
    Signed-off-by: HEIGE-PCloud <heige.pcloud@outlook.com>

--- assets/js/lib/katex.js
+++ assets/js/lib/katex.js
@@ -1,6 +1,6 @@
 import renderMathInElement from 'katex/dist/contrib/auto-render'
 
-if (window.config?.math && document.getElementById('content')) {
+if (window.config?.math) {
   // ignore comment section if exists
   // https://github.com/HEIGE-PCloud/DoIt/issues/487
   if (document.getElementsByClassName('comment')) {
