693e8f5cfc2215648232cb84c79f24a7ccc33aad
PCloud <heige.pcloud@outlook.com>
2024-05-03 14:20:34 +0100

    chore: improve accessbility and SEO

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
         'fgColor-muted' : 'var(--fgColor-muted)',
         'fgColor-link' : 'var(--fgColor-link)',
         'fgColor-link-hover' : 'var(--fgColor-link-hover)',
+        'fgColor-link-muted': 'var(--global-link-color)',
+        'fgColor-link-muted-hover': 'var(--global-link-hover-color)',
         'bgColor-default' : 'var(--bgColor-default)',
         'bgColor-secondary' : 'var(--bgColor-secondary)',
         'bgColor-accent-emphasis' : 'var(--bgColor-accent-emphasis)',
