6623c7984f9a7ab1e10dd42d47b5725f5583253a
PCloud <heige.pcloud@outlook.com>
2024-05-14 16:29:45 +0100

    refactor: math

---
+++ assets/js/lib/mathjax.ts
@@ -0,0 +1,11 @@
+const delimiters = window.config?.math?.delimiters
+if (delimiters) {
+  const displayMath = delimiters.filter(d => d.display).map(d => [d.left, d.right])
+  const inlineMath = delimiters.filter(d => !d.display).map(d => [d.left, d.right])
+  window.MathJax = {
+    tex: {
+      displayMath,
+      inlineMath
+    }
+  };
+}
