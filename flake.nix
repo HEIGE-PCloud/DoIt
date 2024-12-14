bec0b54025f9e11e1ee8aeeb4e57bad83e37f636
PCloud <heige.pcloud@outlook.com>
2024-12-14 22:35:06 +0000

    support nix flake for dev

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Example JavaScript development environment for Zero to Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_23
+            hugo
+          ];
+          shellHook = ''
+            npm install && npm run dev
+          '';
+        };
+      });
+    };
+}
