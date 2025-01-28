404fcfcad446076bd6ba569b0e81b0ef897235dd
WingsZeng <50052878+WingsZeng@users.noreply.github.com>
2025-01-28 17:42:43 +0800

    feat: add support for reaction option and improve texRenderer option in waline (#1432)

--- assets/js/lib/waline.js
+++ assets/js/lib/waline.js
@@ -1,5 +1,18 @@
 import { init } from "@waline/client";
 
 if (window.config?.comment?.waline) {
-  init(window.config.comment.waline);
+  let { texRenderer } = window.config.comment.waline;
+  if (texRenderer) {
+    if (window.katex?.renderToString) {
+      texRenderer = (blockMode, tex) =>
+        window.katex.renderToString(tex, {
+          displayMode: blockMode,
+          throwOnError: false,
+        });
+    } else if (window.MathJax?.tex2mml) {
+      texRenderer = (blockMode, tex) =>
+        window.MathJax.tex2mml(tex, { display: blockMode });
+    }
+  }
+  init({ ...window.config.comment.waline, texRenderer });
 }
