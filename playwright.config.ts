fdcbd5ba99fc342f774115536036b62a971c918c
PCloud <heige.pcloud@outlook.com>
2025-02-21 14:08:44 +0000

    chore: change CI workflow to run on macOS and remove unused snapshot path template

--- playwright.config.ts
+++ playwright.config.ts
@@ -78,7 +78,6 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-  snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}-{projectName}{ext}',
 
   expect: {
     toMatchSnapshot: {
