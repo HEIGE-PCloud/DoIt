36bee94ed6b67c4250a02918cb592dbb738d71a8
PCloud <heige.pcloud@outlook.com>
2025-02-21 15:09:55 +0000

    feat: add Playwright for visual regression testing (#1440)
    * feat: add Playwright for visual regression testing
    
    * feat: update snapshot path template in Playwright configuration
    
    * chore: change CI workflow to run on macOS and remove unused snapshot path template
    
    * update snapshots
    
    * feat: update visual regression tests to use toHaveScreenshot method
    
    * feat: update screenshot expectation to use maxDiffPixelRatio
    
    * feat: remove threshold from screenshot expectation for homepage test
    
    * feat: add visual regression tests for multiple pages
    
    * feat: update Playwright configuration and enhance visual regression tests
    
    * feat: add Hugo setup to Playwright workflow
    
    * feat: update Playwright workflow to install Hugo via Homebrew
    
    * feat: update visual test pages by commenting out unused entries

---
+++ tests/visual.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from "@playwright/test";
+
+const pages = [
+  "",
+  // "posts",
+  "tags",
+  "categories",
+  "series",
+  "authors",
+  "showcase",
+  "about",
+  "aplayer-tests",
+  "extend-shortcodes-tests",
+  "person-tests",
+  "author-fallback-tests",
+  "friend-link-tests",
+  "related-tests",
+  "author-single-tests",
+  "image-tests",
+  "series-taxonomy-tests",
+  // "bilibili-tests",
+  // "katex-tests",
+  "showcase-tests",
+  "bluesky-tests",
+  // "mapbox-tests",
+  "tab-tests",
+  // "builtin-shortcodes-tests",
+  "markdown-tests",
+  "toc-tests",
+  "codeblock-tests",
+  // "mermaid-tests",
+  "typeit-tests",
+  "echarts-tests",
+  // "music-tests",
+];
+
+pages.forEach((path) => {
+  test(`Visual regression for /${path}`, async ({ page }) => {
+    await page.goto(`http://127.0.0.1:1313/${path}`, {
+      waitUntil: "networkidle",
+    });
+
+    await expect(page).toHaveScreenshot({
+      fullPage: true,
+    });
+  });
+});
