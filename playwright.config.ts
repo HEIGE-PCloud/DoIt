e15e2d009482f9891998164c36a1ded95ae4388a
PCloud <heige.pcloud@outlook.com>
2025-02-21 14:54:25 +0000

    feat: update Playwright configuration and enhance visual regression tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -20,9 +20,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: '100%',
+  workers: "100%",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -31,34 +31,34 @@ export default defineConfig({
     viewport: { width: 1280, height: 720 },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
     {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 5"] },
     },
     {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 12"] },
     },
 
     /* Test against branded browsers. */
@@ -73,11 +73,12 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command:
+      "hugo server --source=exampleSite --themesDir ../.. -D --disableFastRender --noHTTPCache --disableLiveReload --disableBrowserError --baseURL=http://127.0.0.1:1313",
+    url: "http://127.0.0.1:1313",
+    reuseExistingServer: !process.env.CI,
+  },
 
   expect: {
     toHaveScreenshot: {
--- tests/visual.spec.ts
+++ tests/visual.spec.ts
@@ -7,12 +7,36 @@ const pages = [
   "categories",
   "series",
   "authors",
-  "docs",
+  "showcase",
+  "about",
+  "aplayer-tests",
+  "extend-shortcodes-tests",
+  "person-tests.md",
+  "author-fallback-tests",
+  "friend-link-tests",
+  "related-tests",
+  "author-single-tests",
+  "image-tests",
+  "series-taxonomy-tests",
+  // "bilibili-tests",
+  // "katex-tests",
+  "showcase-tests",
+  "bluesky-tests.md",
+  "mapbox-tests",
+  "tab-tests",
+  // "builtin-shortcodes-tests",
+  "markdown-tests",
+  "toc-tests",
+  "codeblock-tests",
+  // "mermaid-tests",
+  "typeit-tests",
+  "echarts-tests",
+  // "music-tests",
 ];
 
 pages.forEach((path) => {
   test(`Visual regression for /${path}`, async ({ page }) => {
-    await page.goto(`https://hugodoit.pages.dev/${path}`, {
+    await page.goto(`http://127.0.0.1:1313/${path}`, {
       waitUntil: "networkidle",
     });
 
