0c6a0756696eb9c01c90b0667b8640a4d4b664bb
Stephen Zhang <yicheng123456@outlook.com>
2025-03-13 02:55:55 +0800

    chore: format code with prettier

--- assets/js/lib/plantuml.js
+++ assets/js/lib/plantuml.js
@@ -1,23 +1,23 @@
 if (window.config?.plantuml) {
-    const diagrams = document.querySelectorAll('script[type="plantuml"]');
+  const diagrams = document.querySelectorAll('script[type="plantuml"]');
 
-    diagrams.forEach((diagram) => {
-        const content = diagram.innerHTML;
-        const server = diagram.dataset.server;
-        const format = diagram.dataset.format;
-        const title = diagram.dataset.title;
+  diagrams.forEach((diagram) => {
+    const content = diagram.innerHTML;
+    const server = diagram.dataset.server;
+    const format = diagram.dataset.format;
+    const title = diagram.dataset.title;
 
-        const encoded = plantumlEncoder.encode(content);
-        const img = document.createElement("img");
+    const encoded = plantumlEncoder.encode(content);
+    const img = document.createElement("img");
 
-        img.src = `${server}/${format}/${encoded}`;
-        img.loading = "lazy";
-        img.id = 'plantuml-diagram'
+    img.src = `${server}/${format}/${encoded}`;
+    img.loading = "lazy";
+    img.id = "plantuml-diagram";
 
-        if (title) {
-            img.alt = title;
-        }
+    if (title) {
+      img.alt = title;
+    }
 
-        diagram.replaceWith(img);
-    });
+    diagram.replaceWith(img);
+  });
 }
