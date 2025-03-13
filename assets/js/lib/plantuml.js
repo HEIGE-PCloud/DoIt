7a3a9d9e24f2b61a7a8ac1658bf0688a3a6b21f5
PCloud <heige.pcloud@outlook.com>
2025-03-13 12:36:51 +0000

    feat: support light dark theme switch for plantuml wavedrom

--- assets/js/lib/plantuml.js
+++ assets/js/lib/plantuml.js
@@ -1,23 +1,39 @@
-if (window.config?.plantuml) {
-  const diagrams = document.querySelectorAll('script[type="plantuml"]');
+function initPlantuml() {
+  if (window.config?.plantuml) {
+    const diagrams = document.querySelectorAll('script[type="plantuml"]');
+    const isDark = window.isDark ? "d" : "";
+    diagrams.forEach((diagram) => {
+      const content = diagram.innerHTML;
+      const server = diagram.dataset.server;
+      const format = diagram.dataset.format;
+      const title = diagram.dataset.title;
+      const encoded = plantumlEncoder.encode(content);
+      
+      const img = document.createElement("img");
+      img.dataset.server = server;
+      img.dataset.format = format;
+      img.dataset.encoded = encoded;
+      img.src = `${server}/${isDark}${format}/${encoded}`;
+      img.loading = "lazy";
+      img.className = "plantuml-diagram";
+      img.style.margin = "auto";
+      if (title) {
+        img.alt = title;
+      }
+      diagram.replaceWith(img);
+    });
+  }
+}
 
-  diagrams.forEach((diagram) => {
-    const content = diagram.innerHTML;
+function updatePlantuml() {
+  document.querySelectorAll("img.plantuml-diagram").forEach((diagram) => {
     const server = diagram.dataset.server;
     const format = diagram.dataset.format;
-    const title = diagram.dataset.title;
-
-    const encoded = plantumlEncoder.encode(content);
-    const img = document.createElement("img");
-
-    img.src = `${server}/${format}/${encoded}`;
-    img.loading = "lazy";
-    img.id = "plantuml-diagram";
-
-    if (title) {
-      img.alt = title;
-    }
-
-    diagram.replaceWith(img);
+    const encoded = diagram.dataset.encoded;
+    const isDark = window.isDark ? "d" : "";
+    diagram.src = `${server}/${isDark}${format}/${encoded}`;
   });
 }
+
+initPlantuml();
+window.switchThemeEventSet.add(updatePlantuml);
